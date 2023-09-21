c775256849a3547f5690aa28229f66bda5a64eb9
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-09-21 12:27:53 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -13,3 +13,6 @@ class GetRequester:
     def load_json(self):
         parsed_response = json.loads(self.get_response_body())
         return parsed_response
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
